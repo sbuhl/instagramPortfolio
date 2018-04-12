d5540d39f1a83cee0071e402bc8dddfd6a81da0c
Cerrer <s.buhl@hotmail.com>
2018-04-12 11:02:49 +0200

    Add the instagram flux

--- js/app.js
+++ js/app.js
@@ -9,19 +9,6 @@ $(function() {
         parallax();
     });
  
-    //Set up instafeed
-    var feed = new Instafeed({
-        clientId: 'dbf12bbd983f4ea1a9762d5a0f8bab1e',
-        target: 'instafeed',
-        get: 'tagged',
-        tagName: 'photographyportfolio',
-        links: true,
-        limit: 8,
-        sortBy: 'most-recent',
-        resolution: 'standard_resolution',
-        template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"><div class="photo-box"><div class="image-wrap"><a href="{{link}}"><img src="{{image}}"></a><div class="likes">{{likes}} Likes</div></div><div class="description">{{caption}}<div class="date">{{model.date}}</div></div></div></div>'
-    });
-    feed.run();
- 
+    
+
 });
-// https://www.instagram.com/oauth/authorize/?client_id=dbf12bbd983f4ea1a9762d5a0f8bab1e&redirect_uri=http://macbook-pro-de-cerrer.local:5757/&response_type=token&scope=public_content
